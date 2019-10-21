9a4e7dd5004a259f0dd4c6de4daad77acea49393
Emmanuel Kamala <emmanuelkamala86@gmail.com>
2019-10-21 06:16:36 +0300

    Create events form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,28 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.new(events_params)
+      if @event.save
+      redirect_to(events_path)
+      else
+      render('new')
+      end
+  end
+
+  private
+
+  def events_params
+    params.require(:event).permit(:title, :description, :location, :date)
   end
 end
