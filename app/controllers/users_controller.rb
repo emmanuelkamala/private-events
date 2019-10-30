31d3f7688d9eb356459499c3aa9ca91841860077
emmanuelkamala <emmanuelkamala86@gmail.com>
2019-10-30 18:22:29 +0300

    Create show action and gravatar image for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @user = User.all
+  end
+
   def show
     @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "The user #{@user.username} has been successfully updated"
+      redirect_to (events_path)
+    else
+      render 'edit'
+    end
   end
 
   private 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
     def error_messages_for(object)
-        render(:partial => 'application/error_messages', :locals => {:object => object})
-      end
+      render(:partial => 'application/error_messages', :locals => {:object => object})
+    end
+
+    def gravatar_for(user, options = {size:80})
+     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+     size = options[:size]
+     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+     image_tag(gravatar_url, alt: user.username, class: "img-circle")
+
+    end
 end
